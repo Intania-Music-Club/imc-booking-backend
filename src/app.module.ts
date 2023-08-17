1d612f91a6045980e7715ac08c60efee54982f65
khunfloat <60844731+khunfloat@users.noreply.github.com>
2023-08-17 12:59:17 +0700

    feat: admin/bookingconfirm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AdminModule } from './admin/admin.module';
 import { BookingModule } from './booking/booking.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, BookingModule],
+  imports: [PrismaModule, UserModule, BookingModule, AdminModule],
 })
 export class AppModule {}
