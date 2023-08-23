48f5c294b46e529de7b8ac330a1ba3d9975d8638
thanapatk <22460@tun.ac.th>
2023-08-23 09:37:17 +0700

    refactor: change route name to auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,17 @@ import { ConfigModule } from '@nestjs/config';
 import { AdminModule } from './admin/admin.module';
 import { BookingModule } from './booking/booking.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { UserModule } from './user/user.module';
-import { LoginModule } from './login/login.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     PrismaModule,
-    UserModule,
     BookingModule,
     AdminModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    LoginModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
