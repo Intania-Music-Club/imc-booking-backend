d236b90598003fd8dc1bc545ccd2aed0446bb604
khunfloat <60844731+khunfloat@users.noreply.github.com>
2023-08-23 15:34:00 +0700

    feat: add auth_module to app_module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AdminModule } from './admin/admin.module';
+import { AuthModule } from './auth/auth.module';
 import { BookingModule } from './booking/booking.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
@@ -11,6 +12,7 @@ import { UserModule } from './user/user.module';
     UserModule,
     BookingModule,
     AdminModule,
+    AuthModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
